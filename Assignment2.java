335b08da539da9ead8aabf696176ef684511b217
DiyaR <125185432+Diyarr1001@users.noreply.github.com>
2024-10-16 23:27:03 +0530

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,82 +1,79 @@
-package Assignments;
+package assignment2;
 
-public class Assignment2 {
+public class Queue_Class {
+	private int queue[], Maxsize, f, r, f1, r1, mid;
 
-	 
-	private int[] arr;
-    private int size;
-    private int front1, rear1, front2, rear2;
+	public void create_Queue(int size) {
+		f = mid + 1;
+		r = mid + 1;
+		f1 = r;
+		r1 = f;
+		mid = Maxsize / 2;
+		queue = new int[size];
+		Maxsize = size;
+	}
 
-    public Assignment2(int n) {
-        size = n;
-        arr = new int[n];
-        
-        
-        front1 = rear1 = (n / 2) - 1; 
-        front2 = rear2 = n / 2;        
-    }
+	void enqueue1(int e) {
+		r++;
+		queue[r] = e;
+		System.out.println("Element " + e + "inseretd in queue");
+	}
 
-   
-    public void enqueue1(int data) {
-        if (rear1 < 0) {
-            System.out.println("Queue 1 full");
-            return;
-        }
-        arr[rear1--] = data;
-    }
+	void enqueue2(int e) {
+		r1++;
+		queue[r1] = e;
+		System.out.println("Element " + e + "inseretd in queue");
+	}
 
-   
-    public void enqueue2(int data) {
-        if (rear2 >= size) {
-            System.out.println("Queue 2 full");
-            return;
-        }
-        arr[rear2++] = data;
-    }
+	boolean is_Full1() {
+		if (r == Maxsize - 1)
+			return true;
+		else
+			return false;
+	}
 
-    
-    public void dequeue1() {
-        if (front1 == rear1) {
-            System.out.println("Queue 1 Underflow");
-            return;
-        }
-        System.out.println("Dequeued from Queue 1: " + arr[++front1]);
-    }
+	boolean is_Full2() {
+		if (r1 == Maxsize - 1)
+			return true;
+		else
+			return false;
+	}
 
-    
-    public void dequeue2() {
-        if (front2 == rear2) {
-            System.out.println("Queue 2 Underflow");
-            return;
-        }
-        System.out.println("Dequeued from Queue 2: " + arr[--front2]);
-    }
+	int dequeue1() {
+		int temp = queue[f];
+		f++;
+		return (temp);
+	}
 
-   
-    public void printQueue1() {
-        if (front1 == rear1) {
-            System.out.println("Queue 1 is Empty");
-            return;
-        }
-        System.out.print("Queue 1: ");
-        for (int i = front1 + 1; i <= rear1; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+	int dequeue2() {
+		int temp = queue[f1];
+		f1++;
+		return (temp);
+	}
 
-   
-    public void printQueue2() {
-        if (front2 == rear2) {
-            System.out.println("Queue 2 is Empty");
-            return;
-        }
-        System.out.print("Queue 2: ");
-        for (int i = front2 - 1; i >= rear2; i--) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+	boolean is_Empty1() {
+		if (f > r)
+			return true;
+		else
+			return false;
+	}
+
+	boolean is_Empty2() {
+		if (f1 > r1)
+			return true;
+		else
+			return false;
+	}
+
+	void print_Queue1() {
+		for (int i = f; i <= r; i++) {
+			System.out.println(queue[i]);
+		}
+	}
+
+	void print_Queue2() {
+		for (int i = f1; i <= r1; i++) {
+			System.out.println(queue[i]);
+		}
+	}
 }
-
-
